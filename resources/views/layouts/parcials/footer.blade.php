450899d8ec92c46d75e67a3c87dfa943ab734123
androidgame1 <anas.wawa10@gmail.com>
2022-10-11 04:05:09 -0700

    Doing some modifications

--- resources/views/layouts/parcials/footer.blade.php
+++ resources/views/layouts/parcials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer text-center">
-            © 2022 {{__('messages.hospital_management')}} by <a href="https://www.devanas.com">www.devanas.com</a>
+            © 2022 {{__('messages.hospital_management')}} {{__('messages.by')}} <a href="https://www.devanas.com">www.devanas.com</a>
 </footer>
